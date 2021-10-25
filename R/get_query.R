98e772669782f30da4c8a27571a26522a7bb220d
Emil Bjørn <EMBN@ssi.dk>
2021-10-25 19:08:48 +0200

    Working product with get_query

---
+++ R/get_query.R
@@ -0,0 +1,49 @@
+#' Get query from database
+#'
+#' Execute an input SQL query and close the database connection after
+# 'collection of data
+#'
+#' @param query string
+#'
+#' @return data frame
+#' @importFrom magrittr %>%
+#' @importFrom tibble tibble
+#'
+#' @export
+#' @examples
+#' get_query("SELECT TOP (10)
+#' MatrixPlateBC,
+#' DWPlateBC,
+#' RTimestampPM
+#' FROM PlateFlow_Biomek
+#' ORDER BY RTimestampPM DESC")
+
+
+get_query <- function(query){
+  start_time <- Sys.time()
+
+  conn <- odbc::dbConnect(odbc::odbc(),
+                          driver = "ODBC Driver 17 for SQL Server",
+                          server = "s-dnbdb02-p",
+                          database = "dnb_covid19",
+                          trusted_connection = "Yes",
+                          encoding = "latin1",
+                          timezone = Sys.timezone(),
+                          timezone_out = Sys.timezone())
+
+  d <- odbc::dbGetQuery(conn, query) %>%
+    tibble()
+
+  elapsed <- difftime(Sys.time(), start_time, units = "secs")
+  message("Collected ", nrow(d), " rows in ",
+          ifelse(elapsed > 60,
+                 sprintf("%.0f:%2.0f minutes",
+                         as.numeric(elapsed) %/% 60,
+                         as.numeric(elapsed) %% 60),
+                 sprintf("%.2f seconds",
+                         elapsed)
+          ))
+
+  odbc::dbDisconnect(conn)
+  return(d)
+}
